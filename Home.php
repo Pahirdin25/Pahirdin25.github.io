1a6aa1e56c0b20221e2930a4c078771775b135dc
SaltFish <106537254+Pahirdin25@users.noreply.github.com>
2023-07-25 01:25:55 +0800

    Update and rename Home.html to Home.php

---
+++ Home.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        #box{height: 200px;width: 300px; margin: auto; margin-top: 100px;}
+        h2{margin: auto; margin-top: 100px;}
+    </style>
+</head>
+<body>
+<h2 align="center">管理员</h2>
+<div id="box">
+
+    <form action="test_10.php" method="post" align="center">
+        用户: <input type="text" name="user_name"><br><br>
+        密码: <input type="password" name="user_password"><br><br>
+        <input type="submit" value="提交">
+    </form>
+</div>
+
+</body>
+</html>
